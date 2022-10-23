893fe887705d932e078193fec79141b561975b3e
Dmitry <42121541+dkpost3@users.noreply.github.com>
2022-10-23 10:41:59 +0300

    Add files via upload

---
+++ twrp_X687.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from Infinix-X687 device
+$(call inherit-product, device/infinix/X687/device.mk)
+
+PRODUCT_DEVICE := X687
+PRODUCT_NAME := twrp_X687
+PRODUCT_BRAND := Infinix
+PRODUCT_MODEL := Infinix X687
+PRODUCT_MANUFACTURER := infinix
+
+PRODUCT_GMS_CLIENTID_BASE := android-infinix
